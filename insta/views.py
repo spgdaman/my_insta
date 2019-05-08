e73b850fa7fa785faaf51d243fd584bfb4e068e1
simon <gatheru.simon432@gmail.com>
2019-05-08 17:00:51 +0300

    url and view for index page defined and tested

--- insta/views.py
+++ insta/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def hello(request):
-    return render(request,'welcome.html')
-
+def main(request):
+    return render(request, 'index.html')
